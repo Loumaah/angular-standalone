c6028d37de8ec8202521ab2fe32aab4dc32abd8d
marlou <marielou.jacob03@gmail.com>
2025-12-03 11:48:35 +0100

    Manage errors

--- src/app/components/personne-module/personne/personne.ts
+++ src/app/components/personne-module/personne/personne.ts
@@ -14,21 +14,35 @@ import { PersonneFormComponent } from '../personne-form/personne-form';
 export class PersonneComponent implements OnInit {
 
   personnes = signal<Personne[]>([]);
+  erreur = signal<string | null>(null)
   personne: Personne = { nom: '', prenom: '', age: 0 };
   
   // constructor(private ps: PersonneService) {}
   ps = inject(PersonneService);
 
   ngOnInit(): void {
-    this.ps.findAll().subscribe(res => {
-      this.personnes.set(res);
+    this.ps.findAll().subscribe({
+      next: res => {
+        this.personnes.set(res);
+      },
+      error: err => {
+        this.erreur.set("Problème de récupération de données");
+        console.error('Erreur lors de la récupération des personnes', err);
+      }
     });
     this.personne = { nom: '', prenom: '', age: 0 };
   }
 
   ajouter() {
-    this.ps.save(this.personne).subscribe(res => {
-      this.personnes.set([...this.personnes(), res]);
+    this.ps.save(this.personne).subscribe({
+      next: p => {
+        this.personnes.set([...this.personnes(), p])        
+        this.personne = { nom: '', prenom: '', age: 0 }
+      },
+      error: (err) => {
+        this.erreur.set("Insertion impossible.")
+        console.error('Erreur lors de l\'ajout de la personne', err);      
+      }
     });
   }
 
